ce0cb2c26667de7c1e42a427ddbcda999650c14b
Eduer Jaramillo <92195584+EDUERDEJESUSJARAMILLOBALBIN@users.noreply.github.com>
2023-06-06 00:58:06 -0500

    Revert "Revert "Commit""
    This reverts commit 04ab8ee81ce98c89cfcd443df681405ea27bd976.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,11 @@ class Ticket
   include Mongoid::Document
   include Mongoid::Timestamps
   field :day, type: String
+  field :spring, type: String
   field :date, type: Date
   field :remaining, type: String
+  field :logged_time, type: String
+  field :status, type: String
   field :description_remaining, type: String
   field :branch_detail, type: String
 end
